1a7bf22afeeb87b082f43025b52bbe88cfe84d8c
bikash1376 <139378098+bikash1376@users.noreply.github.com>
2024-07-19 05:39:01 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,6 @@
 "use client";
 
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import InstagramIcon from "@mui/icons-material/Instagram";
 import Link from "next/link";
 
 const Footer = () => {
@@ -22,152 +18,53 @@ const Footer = () => {
             </p>
             <div className="flex mt-8 space-x-6">
               <Link
-                href="https://www.facebook.com/profile.php?id=100017192357822&sk"
-                target="_blank"
-              >
-                <FacebookOutlinedIcon className="hover:text-rose-600 hover:-translate-y-1 transition-all" />
-              </Link>
-              <Link href="https://github.com/NaseemKhan005" target="_blank">
-                <GitHubIcon className="hover:text-rose-600 hover:-translate-y-1 transition-all" />
-              </Link>
-              <Link
                 href="https://www.linkedin.com/in/naseem-khan-275275258/"
                 target="_blank"
               >
                 <LinkedInIcon className="hover:text-rose-600 hover:-translate-y-1 transition-all" />
-              </Link>
-              <Link href="https://twitter.com/NaseemK69128903" target="_blank">
-                <TwitterIcon className="hover:text-rose-600 hover:-translate-y-1 transition-all" />
-              </Link>
-              <Link
-                href="https://www.instagram.com/naseem_khan005/"
-                target="_blank"
-              >
-                <InstagramIcon className="hover:text-rose-600 hover:-translate-y-1 transition-all" />
               </Link>
             </div>
           </div>
 
           <div className="grid gap-8 lg:col-span-2 grid-cols-2 md:grid-cols-4">
             <div>
-              <p className="font-medium">Company</p>
+              <p className="font-medium">Links</p>
               <nav className="flex flex-col mt-4 space-y-2 text-sm">
                 <Link
                   href="/"
                   className="hover:opacity-75 hover:border-b hover:border-neutral-400 w-fit"
                 >
-                  About
+                  Home
                 </Link>
                 <Link
-                  href="/"
+                  href="/why-choose-math-easy"
                   className="hover:opacity-75 hover:border-b hover:border-neutral-400 w-fit"
                 >
-                  Meet the Team
+                  Why Choose Math Easy
                 </Link>
                 <Link
-                  href="/"
+                  href="/about-math-easy"
                   className="hover:opacity-75 hover:border-b hover:border-neutral-400 w-fit"
                 >
-                  History
+                  About Math Easy
                 </Link>
                 <Link
-                  href="/"
+                  href="/curriculum"
                   className="hover:opacity-75 hover:border-b hover:border-neutral-400 w-fit"
                 >
-                  Careers
-                </Link>
-              </nav>
-            </div>
-            <div>
-              <p className="font-medium">Services</p>
-              <nav className="flex flex-col mt-4 space-y-2 text-sm ">
-                <Link
-                  href="/"
-                  className="hover:opacity-75 hover:border-b hover:border-neutral-400 w-fit"
-                >
-                  Coaching
+                  Curriculum
                 </Link>
                 <Link
-                  href="/"
+                  href="/reviews"
                   className="hover:opacity-75 hover:border-b hover:border-neutral-400 w-fit"
                 >
-                  Company Review
-                </Link>
-                <Link
-                  href="/"
-                  className="hover:opacity-75 hover:border-b hover:border-neutral-400 w-fit"
-                >
-                  Accounts Review
-                </Link>
-                <Link
-                  href="/"
-                  className="hover:opacity-75 hover:border-b hover:border-neutral-400 w-fit"
-                >
-                  HR Consulting
-                </Link>
-                <Link
-                  href="/"
-                  className="hover:opacity-75 hover:border-b hover:border-neutral-400 w-fit"
-                >
-                  SEO Optimisation
-                </Link>
-              </nav>
-            </div>
-            <div>
-              <p className="font-medium">Helpful Links</p>
-              <nav className="flex flex-col mt-4 space-y-2 text-sm ">
-                <Link
-                  href="/"
-                  className="hover:opacity-75 hover:border-b hover:border-neutral-400 w-fit"
-                >
-                  Contact
-                </Link>
-                <Link
-                  href="/"
-                  className="hover:opacity-75 hover:border-b hover:border-neutral-400 w-fit"
-                >
-                  FAQs
-                </Link>
-                <Link
-                  href="/"
-                  className="hover:opacity-75 hover:border-b hover:border-neutral-400 w-fit"
-                >
-                  Live Chat
-                </Link>
-              </nav>
-            </div>
-            <div>
-              <p className="font-medium">Legal</p>
-              <nav className="flex flex-col mt-4 space-y-2 text-sm ">
-                <Link
-                  href="/"
-                  className="hover:opacity-75 hover:border-b hover:border-neutral-400 w-fit"
-                >
-                  Privacy Policy
-                </Link>
-                <Link
-                  href="/"
-                  className="hover:opacity-75 hover:border-b hover:border-neutral-400 w-fit"
-                >
-                  Terms &amp; Conditions
-                </Link>
-                <Link
-                  href="/"
-                  className="hover:opacity-75 hover:border-b hover:border-neutral-400 w-fit"
-                >
-                  Returns Policy
-                </Link>
-                <Link
-                  href="/"
-                  className="hover:opacity-75 hover:border-b hover:border-neutral-400 w-fit"
-                >
-                  Accessibility
+                  Reviews
                 </Link>
               </nav>
             </div>
           </div>
         </div>
-        <p className="mt-8 text-xs">© 2022 Naseem Khan - All rights reserved</p>
+        <p className="mt-8 text-xs">© 2024 Math Easy - All rights reserved</p>
       </div>
     </footer>
   );
